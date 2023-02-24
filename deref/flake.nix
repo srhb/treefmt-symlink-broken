50b0595e1e4eb6f155f9e5e29a7ba3be22718e26
Sarah Brofeldt <sarah@qtr.dk>
2023-02-24 09:59:56 +0100

    demo

---
+++ deref/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  description = "treefmt-nix broken on flakes";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [inputs.treefmt-nix.flakeModule];
+      perSystem = {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.alejandra.enable = true;
+        };
+      };
+    };
+}
