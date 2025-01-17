d14b332974ab3737ffbfb086f914d5c560b2e9a4
Sarah Brofeldt <sarah@qtr.dk>
2025-01-17 12:06:53 +0100

    treefmt: specify package

--- deref/flake.nix
+++ deref/flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt.url = "github:numtide/treefmt/v2.0.0";
   };
 
   description = "treefmt-nix broken on flakes";
@@ -14,8 +15,9 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
       imports = [inputs.treefmt-nix.flakeModule];
-      perSystem = {
+      perSystem = { inputs', ... }: {
         treefmt = {
+          package = inputs'.treefmt.packages.default;
           projectRootFile = "flake.nix";
           programs.alejandra.enable = true;
         };
