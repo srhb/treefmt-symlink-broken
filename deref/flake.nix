0d3fda9be27190599f21fa67ee8833238b571d19
Sarah Brofeldt <sarah@qtr.dk>
2025-01-17 17:37:08 +0100

    nix 2.24.11

--- deref/flake.nix
+++ deref/flake.nix
@@ -4,7 +4,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt.url = "github:numtide/treefmt/v2.1.1";
-    nix.url = "github:nixos/nix/2.25.4";
+    nix225.url = "github:nixos/nix/2.25.4";
+    nix224.url = "github:nixos/nix/2.24.11";
   };
 
   description = "treefmt-nix broken on flakes";
@@ -24,7 +25,8 @@
         };
         devShells.default = pkgs.mkShell {
           packages = [
-            # inputs'.nix.packages.default
+            # inputs'.nix225.packages.default
+            inputs'.nix224.packages.default
           ];
         };
       };
